5d1b92f046ec257492974ba08a45d3553da23aa8
ChickenTracks <reiser999@yahoo.com>
2026-01-09 13:37:43 -0700

    Initialize React application with ChickenTracks component

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ChickenTracks from './ChickenTracks_v5';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ChickenTracks />
+  </React.StrictMode>
+);
